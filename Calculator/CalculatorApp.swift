57676d92dd1424e7aad33143ebfd1d40ebe61b00
Arvind S <arvindsunil7@gmail.com>
2024-01-10 20:47:52 +0530

    First commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by ARVIND SUNIL on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
